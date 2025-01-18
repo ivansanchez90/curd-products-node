45a0c5347737677329d29481dcb1dc9edba15228
ivansanchez90 <sanchezoliva.ivan@gmail.com>
2025-01-18 16:30:37 -0300

    get product por id y udpate por id

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -12,6 +12,34 @@ export const getProducts = async (req: Request, res: Response) => {
     console.log(error)
   }
 }
+export const getProductById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params
+    const product = await Product.findByPk(id)
+    if (!product) {
+      return res.status(404).json({ error: 'Producto No Encontrado' })
+    }
+    res.json({ data: product })
+  } catch (error) {
+    console.log(error)
+  }
+}
+export const updateProduct = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params
+    const product = await Product.findByPk(id)
+    if (!product) {
+      return res.status(404).json({ error: 'Producto No Encontrado' })
+    }
+
+    await product.update(req.body, { where: { id: id } })
+    await product.save()
+
+    res.json({ data: product })
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 export const createProduct = async (req: Request, res: Response) => {
   try {
--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,47 @@
 import { Router } from 'express'
-import { createProduct, getProducts } from './handlers/product'
-import { body, validationResult } from 'express-validator'
+import {
+  createProduct,
+  getProductById,
+  getProducts,
+  updateProduct,
+} from './handlers/product'
+import { body, validationResult, param } from 'express-validator'
 import { handleInputErrors } from './middleware'
 
 const router = Router()
 // Routing
 router.get('/', getProducts)
+router.get(
+  '/:id',
+  param('id').isInt().withMessage('ID no válido'),
+  handleInputErrors,
+  (req, res) => {
+    getProductById(req, res)
+  }
+)
+
+router.put(
+  '/:id',
+  param('id').isInt().withMessage('ID no válido'),
+  body('name')
+    .notEmpty()
+    .withMessage('El nombre del producto no puede ir vacío'),
+
+  body('price')
+    .isNumeric()
+    .withMessage('Valor no válido')
+    .notEmpty()
+    .withMessage('El precio del producto no puede ir vacío')
+    .custom((value) => value > 0)
+    .withMessage('Precio no válido'),
+  body('availability')
+    .isBoolean()
+    .withMessage('Valor de disponibilidad no válido'),
+  handleInputErrors,
+  (req, res) => {
+    updateProduct(req, res)
+  }
+)
 
 router.post(
   '/',
