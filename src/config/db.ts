ecc0c0051ac9d9c9836e7c295358a8cf54bd5300
ivansanchez90 <sanchezoliva.ivan@gmail.com>
2025-02-07 11:05:18 -0300

    cambio base de datos a render

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,15 +2,10 @@ import { Sequelize } from 'sequelize-typescript'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const db = new Sequelize(
-  process.env.POSTGRES_DB,
-  process.env.POSTGRES_USER,
-  process.env.POSTGRES_PASSWORD,
-  {
-    dialect: 'postgres',
-    models: [__dirname + '/../models/**/*'],
-    logging: false,
-  }
-)
+const db = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  models: [__dirname + '/../models/**/*'],
+  logging: false,
+})
 
 export default db
